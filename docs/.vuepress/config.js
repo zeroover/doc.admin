b9f5463a2e85604b8fb945db94ece62f111cab28
zz <zerodegreecola@gmail.com>
2021-03-05 11:43:44 +0800

    3

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,12 +16,12 @@ module.exports = {
   },
   themeConfig: {
     repo: 'zeroover/doc.admin',
-    repoLabel: '查看源码',
+    //repoLabel: '查看源码',
 
     // 以下为可选的编辑链接选项
 
     // 假如你的文档仓库和项目本身不在一个仓库：
-    docsRepo: 'vuejs/vuepress',
+    docsRepo: 'zeroover/doc.admin',
     // 假如文档不是放在仓库的根目录下：
     docsDir: 'docs',
     // 假如文档放在一个特定的分支下：
